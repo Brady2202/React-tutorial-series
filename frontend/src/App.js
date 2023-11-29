453d2325aeda27622d99a8ca1c1a9cb82256f041
Brady2202 <102337821+Brady2202@users.noreply.github.com>
2023-11-29 17:21:34 -0600

    first-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+/*
+function App() {
+  return (
+    <div className="App">
+       Hello world
+    </div>
+  );
+}
+*/
+const App = () =>  
+  <h1 className='App'> Hello world</h1>
+ 
+
+export default App;
